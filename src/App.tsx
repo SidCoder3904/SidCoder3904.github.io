f1388278583d5c8ee0e1768aa393ae5176d16f07
Siddharth Verma <siddharthverma3904@gmail.com>
2024-01-24 19:14:46 +0530

    added loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import Info from "./components/Info";
 import Intro from "./components/Intro";
+import { useState, useEffect } from "react";
+import Loading from "./components/Loading";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
-    <div className="flex-wrap h-full w-full absolute bg-palette4 overflow-auto font-Raleway select-none scroll-smooth">
+    <div className="flex-wrap h-full w-full absolute bg-palette4 bg-gradient-to-t from-palette4 via-transparent to-palette4 overflow-auto font-Raleway select-none scroll-smooth">
       <Intro />
       <Info />
     </div>
